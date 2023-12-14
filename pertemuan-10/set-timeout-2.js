f29107608f8977a762d632e2c1ef7cf14105e458
jovinagustari <jovinagustari17@gmail.com>
2023-12-14 22:50:55 +0700

    Pertemuan 10

---
+++ pertemuan-10/set-timeout-2.js
@@ -0,0 +1,8 @@
+console.log("Jisung membuka browser Chrome");
+
+setTimeout(()=>{
+    console.log("Mendownload file 1 jam");
+    console.log("Download selesai");
+},5000);
+
+console.log("Jisung membuka Youtube");
